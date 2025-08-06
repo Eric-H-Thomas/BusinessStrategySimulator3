058d8ff1931840cf9e19707e206a6192746f6f17
Eric-H-Thomas <103595220+Eric-H-Thomas@users.noreply.github.com>
2025-08-06 11:59:59 -0600

    use pragma once in config validator

--- WorkingFiles/Config/ConfigValidator.h
+++ WorkingFiles/Config/ConfigValidator.h
@@ -1,8 +1,5 @@
-#ifndef CONFIG_VALIDATOR_H
-#define CONFIG_VALIDATOR_H
+#pragma once
 
 #include <string>
 
 void validate_config(const std::string& strConfigFilePath);
-
-#endif // CONFIG_VALIDATOR_H
